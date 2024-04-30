4c6db655faafb2af7798d1956a7d5fea67296d80
MacWeb <mac.web.company@gmail.com>
2024-04-30 18:38:06 -0400

    MacWeb May Update!!!

--- app.js
+++ app.js
@@ -1,18 +1,20 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const wrap = document.getElementById("wrap");
   const bar = document.getElementById("sidebar");
+  const overflowMenu = document.getElementById("overflow-menu");
+  document.addEventListener("click", function () {
+    bar.classList.remove("movingbar");
+  });
+  document.addEventListener("click", (e) => {
+    if (e.target.classList.contains("shoot")) {
+      overflowMenu.classList.add("menu-slide");
+    } else {
+      overflowMenu.classList.remove("menu-slide");
+    }
+  });
   document.addEventListener("mousemove", function (event) {
     if (event.clientX <= 5 && bar.classList.contains("movingbar") !== true) {
       bar.classList.add("movingbar");
     }
-  });
-
-  document.addEventListener("click", function () {
-    bar.classList.remove("movingbar");
-  })
-
-  wrap.addEventListener("click", function () {
-    console.log("sl");
   });
 });
 
